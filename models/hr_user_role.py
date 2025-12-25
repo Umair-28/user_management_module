a0fb91c7c085ab3fc7e9f2b9ac78469c25eaf63b
Umair-28 <h.umairamjad.30@gmail.com>
2025-12-25 15:41:41 +0500

    new code

--- models/hr_user_role.py
+++ models/hr_user_role.py
@@ -16,8 +16,37 @@ class HrUserRole(models.Model):
         string="Access Rights"
     )
     active = fields.Boolean(default=True)
-    color = fields.Integer(string="Color Index")
+    color = fields.Integer(string="Color Index", default=0)
+    user_count = fields.Integer(
+        string="Number of Users",
+        compute="_compute_user_count"
+    )
 
     _sql_constraints = [
         ('name_unique', 'unique(name)', 'Role name must be unique!')
     ]
+
+    @api.depends('groups_id')
+    def _compute_user_count(self):
+        """Count users who have this role's groups"""
+        for role in self:
+            if role.groups_id:
+                # Count users who have at least one group from this role
+                users = self.env['res.users'].search([
+                    ('groups_id', 'in', role.groups_id.ids)
+                ])
+                role.user_count = len(users)
+            else:
+                role.user_count = 0
+
+    def action_view_users(self):
+        """View users with this role"""
+        self.ensure_one()
+        return {
+            'name': f'Users with {self.name} Role',
+            'type': 'ir.actions.act_window',
+            'res_model': 'res.users',
+            'view_mode': 'tree,form',
+            'domain': [('groups_id', 'in', self.groups_id.ids)],
+            'context': {'create': False}
+        }
