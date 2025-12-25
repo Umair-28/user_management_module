b22f0b2dc950e9dae889593ae2587e41c34fcaf8
Umair-28 <h.umairamjad.30@gmail.com>
2025-12-25 15:37:48 +0500

    remove groups

--- __manifest__.py
+++ __manifest__.py
@@ -25,7 +25,7 @@ Features:
     ],
     "data": [
         "security/ir.model.access.csv",
-        "data/hr_user_role_data.xml",
+        # "data/hr_user_role_data.xml",
         "views/create_user_wizard_view.xml",
         "views/hr_employee_view.xml",
         "views/hr_user_role_views.xml",
